650923fb66320ffd4aa8193fca3e9350f519c8c1
srpallab <pallab.alif@gmail.com>
2023-03-28 02:43:30 +0600

    ADD: Wait for DB commad to Github Action.

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -11,12 +11,10 @@ from django.core.management.base import BaseCommand
 
 class Command(BaseCommand):
     """Django command to wait for database."""
-
     def handle(self, *args, **options):
         """Entrypoint for command."""
         self.stdout.write('Waiting for database...')
         db_up = False
-        
         while db_up is False:
             try:
                 self.check(databases=['default'])
