366c8a421d0ecb81cb8ad62254f226158adcfe42
srpallab <pallab.alif@gmail.com>
2023-04-09 21:51:06 +0600

    ADD: User app.

--- app/app/urls.py
+++ app/app/urls.py
@@ -18,7 +18,7 @@ from drf_spectacular.views import (
     SpectacularSwaggerView,
 )
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 
 urlpatterns = [
@@ -30,4 +30,5 @@ urlpatterns = [
         SpectacularSwaggerView.as_view(url_name='api-schema'),
         name='api-docs',
     ),
+    path('api/user/', include('user.urls')),
 ]
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,3 +1,6 @@
+"""
+Tests for models.
+"""
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 
