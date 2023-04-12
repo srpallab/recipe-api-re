641923a16d03374e3c99598603e088584798db85
srpallab <pallab.alif@gmail.com>
2023-04-13 01:31:40 +0600

    ADD: Recipe Api full.

--- app/core/models.py
+++ app/core/models.py
@@ -1,7 +1,7 @@
 """
 Database Models
 """
-
+from django.conf import settings
 from django.db import models
 from django.contrib.auth.models import (
     AbstractBaseUser,
@@ -43,3 +43,22 @@ class User(AbstractBaseUser, PermissionsMixin):
     objects = UserManager()
 
     USERNAME_FIELD = 'email'
+
+    
+class Recipe(models.Model):
+    """Recipe object."""
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    time_minutes = models.IntegerField()
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    link = models.CharField(max_length=255, blank=True)
+    # tags = models.ManyToManyField('Tag')
+    # ingredients = models.ManyToManyField('Ingredient')
+    # image = models.ImageField(null=True, upload_to=recipe_image_file_path)
+
+    def __str__(self):
+        return self.title
