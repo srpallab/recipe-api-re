8e28191e7a900513bdb8374b5f8c159f0aec8da2
srpallab <pallab.alif@gmail.com>
2023-04-25 11:09:49 +0600

    ADD: Tags to recipe.

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -3,6 +3,7 @@ Views for the recipe APIs
 """
 from rest_framework import (
     viewsets,
+    mixins,
 )
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
@@ -10,6 +11,7 @@ from rest_framework.permissions import IsAuthenticated
 
 from core.models import (
     Recipe,
+    Tag,
 )
 from recipe import serializers
 
@@ -37,3 +39,20 @@ class RecipeViewSet(viewsets.ModelViewSet):
     def perform_create(self, serializer):
         """Create a new recipe."""
         serializer.save(user=self.request.user)
+
+
+class TagViewSet(
+        mixins.DestroyModelMixin,
+        mixins.UpdateModelMixin,
+        mixins.ListModelMixin,
+        viewsets.GenericViewSet
+):
+    """Manage tags in the database."""
+    serializer_class = serializers.TagSerializer
+    queryset = Tag.objects.all()
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        """Filter queryser to authenticated user."""
+        return self.queryset.filter(user=self.request.user).order_by("-name")
